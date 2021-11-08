e03bcb1c1742f78c7df4fcab5ae6f683a94bff5f
ignaciolincolao <ilincolaovenegas@gmail.com>
2021-11-08 17:59:24 -0300

    fix

--- base/script_test.bash
+++ base/script_test.bash
@@ -1,6 +1,5 @@
-blockes=(32 64 128 256)
-threads=(1 32 64 85)
-coolingrates=(0.98 0.998 0.9998)
+coolingrates=(0.98 0.989 0.998)
+temp_inital=(100000 1000 10)
 
 mkdir "./save_example_15_30_25"
 cd ./build
@@ -12,7 +11,9 @@ do
         mkdir "../save_example_15_30_25/rate_$rate"
         echo "---------######## Estado 15 30 25 ####### En rate_$rate donde $repeat de 30 ######----------"
         ((seed+=$repeat*2))
-        ./s3e2_sas 15 30 25 $rate 0.0 100000 0.00000009 1 1 "../save_example_15_30_25/rate_$rate/" "$repeat" $seed
+# Orden de los parametros
+# ejecutable | temp_ini | min_temp | alpha1 | alpha2 | alpha3 | coolingRate | k_reheating | e_const | n_reheating | len1 | len2 | len3 | len4 | Th | n_block | n_thread | rutaSave | prefijo | name_exp
+        ./s3e2_sas 100000 0.00000009 15 30 25 $rate 0.0   1 1 "../save_example_15_30_25/rate_$rate/" "$repeat" $seed
     done
 done
 echo "---------######## Completado 15 30 25 ####### Completado rate_$rate donde $repeat de 30 ######----------"
